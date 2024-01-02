749ea71c182495ae993fdf00f56ed5c2bb40e0e0
GYURI-KIM <jnkgr0527@naver.com>
2024-01-02 17:34:19 +0900

    chdir

---
+++ 데이터_분석을_위한_파이썬/mod1.py
@@ -0,0 +1,8 @@
+# mod1.py 모듈
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
---
+++ 데이터_분석을_위한_파이썬/mod2.py
@@ -0,0 +1,11 @@
+# mod1.py 모듈
+
+def add2(a, b):
+    return a+b
+
+def sub2(a, b):
+    return a-b
+
+if __name__ == "__main__":
+    print(add2(5, 9))
+    print(sub2(7, 3))
